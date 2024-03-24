3804c80ae5997587e620802f553ad28b1dcd6262
Timiemmy <faith.bolanle@yahoo.com>
2024-03-24 12:18:11 +0100

    Added permission to views

--- todoapi/views.py
+++ todoapi/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import TodoSerializer
 from todoapp.models import Todo
 
